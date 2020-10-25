91770f26fc638e97e294071d35d8be60e52ea7f4
AlexeyVolchenkov <altay197@yandex.ru>
2020-10-25 17:58:47 +0300

    operation -

--- Hello.world.py
+++ Hello.world.py
@@ -1,8 +1,10 @@
 print('Hello world!')
 number_1 = int(input('Введите первое число:'))
 number_2 = int(input('Введите второе число:'))
-function = input('Выберите операцию (+, *):')
+function = input('Выберите операцию (+, *, -):')
 if function == '+':
     print(number_1 + number_2)
 elif function == '*':
-    print(number_1*number_2)
+    print(number_1 * number_2)
+elif function == '-':
+    print(number_1 - number_2)
